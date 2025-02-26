d2f62e9f78ef8b28f24e1e3ddbd8373e7bb7f88f
Drenmax7 <92438713+Drenmax7@users.noreply.github.com>
2025-02-26 17:23:19 +0100

    import inutile

--- api/views/partSocial.py
+++ api/views/partSocial.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated,IsAdminUser 
 
 from django.core import exceptions
-from django.db.utils import DataError
 
 from .generalFunctions import filtreTable, updateTable
 from ..models import PartSocial,Societaire
