127ee0f3f6a92cde6960693e9560606933050f20
Drenmax7 <92438713+Drenmax7@users.noreply.github.com>
2025-02-28 12:04:07 +0100

    ajout numero societaire, college

--- api/models/societaire.py
+++ api/models/societaire.py
@@ -3,4 +3,9 @@ from django.db import models
 class Societaire(models.Model):
     id_societaire = models.AutoField(primary_key=True)
     organisation = models.CharField(max_length=50, blank=True, null=True)
+    numero_societaire = models.CharField(max_length=50)
     id_utilisateur = models.OneToOneField('Utilisateur', on_delete=models.CASCADE)
+    college = models.ForeignKey('College', on_delete=models.CASCADE)
+
+class College(models.Model):
+    nom = models.CharField(max_length=50, primary_key=True)
